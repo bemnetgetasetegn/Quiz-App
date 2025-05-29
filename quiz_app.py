7343542144e8ec50d83163048f7192c1ef0d16bf
bemnetgetasetegn <bigbemnet@gmail.com>
2025-05-29 14:20:46 +0300

    finished the game only left with error handling

--- quiz_app.py
+++ quiz_app.py
@@ -15,18 +15,34 @@ class Fetch:
             return []
 
 
-class Quiz_details:
-    def __init__(self, category, difficulty, type):
-        self.category = category
-        self.type = type
-        self.difficulty = difficulty
-    
+
 class Game:
-    def get_user_choices(self):
+    def __init__(self):
+        self.category = ''
+        self.difficulty = ''
+        self.type = False
+  
+    def get_user_category_choices(self):
         fetch_category = Fetch('https://opentdb.com/api_category.php', 'trivia_categories')
         category = fetch_category.fetch_request()
-        for items in category:
-            print(f"{items.id}, {items.name}")
+        for item in category:
+            item_id = item['id']
+            item_name = item['name']
+            print(f"{item_id}, {item_name}")
+        self.category = input('Select a category: ')
+    
+    def get_user_difficulty(self):
+        print('----- select difficulity -----')
+        self.difficulty = input('Easy, Medium, Hard: ').lower()
+
+    def get_user_type(self):
+        print('------ select the type of you question: Multiple choice or True/false --------')
+        self.type = input()
+
+
+
+
 
 start_game = Game()
-start_game.get_user_choices()
+start_game.get_user_category_choices()
+start_game.get_user_difficulty()
